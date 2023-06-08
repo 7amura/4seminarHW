5ac769be81f9a43c7ca6dfe1294f1087cd5a8237
Daria <dasha.shershneva.97@mail.ru>
2023-06-08 15:21:56 +0300

    task

--- 1st.py
+++ 1st.py
@@ -3,37 +3,15 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества. 
 # Затем пользователь вводит сами элементы множеств.
 
-# n = (int(input("Введите число N элементов: ")))
-# num_list_1=[]
-# for i in range(n):
-#     num = int(input("Введите num: "))
-#     num_list_1.append(num)
-# print(num_list_1)
-
-# m = (int(input("Введите число M элементов: ")))
-# num_list_2 = []
-# for i in range(m):
-#     num = int(input("Введите num "))
-#     num_list_2.append(num)
-# print(num_list_2)
-
-# num_list3 = num_list_1+num_list_2
-# print(num_list3)
-
-# checked_nums_list = []
-# for i in num_list3:
-#     if num_list3.count(i) > 1 and i not in checked_nums_list:
-#         checked_nums_list.append(i)
-#         print(i)
-
 
 from random import randint
-n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
+n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов множества N: '))))
 print(n_set)
-m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
+m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов множества M: '))))
 print(m_set)
 s_set = sorted(n_set.intersection(m_set))
 print(*s_set)
+
 
 
 
